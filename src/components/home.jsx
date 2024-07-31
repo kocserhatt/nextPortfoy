bbd0ae422842984f5c69344bbd69b28d7a718324
serhat koç <kocserhat671@gmail.com>
2024-07-31 18:05:10 +0300

    portfoy transferi

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 export default function HomePage() {   
     return (
         <div id="anasayfa" className="home">
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 export default function Navbar() {
     return (
         <div className="container">
