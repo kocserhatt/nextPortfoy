bb71b08554fe7072b3de9b37e56d2785a2b73da4
serhat koç <kocserhat671@gmail.com>
2024-07-31 21:29:42 +0300

    portfoy transferi

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,12 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function HomePage() {   
     return (
         <div id="anasayfa" className="home">
             <div className="left">
-                <span className="home-span">Herkese merhaba&apos;, ben</span>
+                <span className="home-span">Herkese merhaba, ben</span>
                 <h1 className="home-header">Serhat Koç</h1>
-                <p className="home-paragraph">Bu da benim portföy projem&rsquo;. Benim hakkımda bilgi edinmek istersen&apos;, Hakkımda kısmını okuyabilir ya da CV mi indirerek özgeçmişim hakkında bilgi sahibi olabilirsin&rsquo;.</p>
+                <p className="home-paragraph">Bu da benim portföy projem. Benim hakkımda bilgi edinmek istersen, Hakkımda kısmını okuyabilir ya da CV mi indirerek özgeçmişim hakkında bilgi sahibi olabilirsin.</p>
                 <div className="btns">
                     <a href="/serhat koç özgeçmiş.pdf" download="" className="btn">CV yi indir</a>
                     <a href="#" className="btn btn-ghost">Explore More</a>
