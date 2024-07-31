daeab0351af0f1b451125fe8d3b0a7b42d0f483f
serhat koç <kocserhat671@gmail.com>
2024-07-31 17:15:47 +0300

    portfoy transferi

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
                 <h1 className="home-header">Serhat Koç</h1>
                 <p className="home-paragraph">Bu da benim portföy projem. Benim hakkımda bilgi edinmek istersen, 'Hakkımda' kısmını okuyabilir ya da CV'mi indirerek özgeçmişim hakkında bilgi sahibi olabilirsin.</p>
                 <div className="btns">
-                    <a href="/projects/serhat koç özgeçmiş.pdf" download="" className="btn">CV'yi indir</a>
+                    <a href="/serhat koç özgeçmiş.pdf" download="" className="btn">CV'yi indir</a>
                     <a href="" className="btn btn-ghost">Explore More</a>
                 </div>
             </div>
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -8,42 +8,42 @@ export default function Projects(){
             <div className="cards">
                 <a href="https://kocserhatt.github.io/daily/">
                     <div className="card">   
-                        <img className="img-2" src="/projects/Ekran görüntüsü 2024-04-08 173053.png" alt=""/>
+                        <img className="img-2" src="Ekran görüntüsü 2024-04-08 173053.png" alt=""/>
                         <h2 className="h2-1">Daily</h2>
                         <p className="p-1">Günlük Web Site</p>
                     </div>
                 </a>
                 <a href="https://kocserhatt.github.io/e-ticaret-sitesi/">
                     <div className="card">   
-                        <img className="img-2" src="/projects/Ekran görüntüsü 2024-04-08 173135.png" alt=""/>
+                        <img className="img-2" src="Ekran görüntüsü 2024-04-08 173135.png" alt=""/>
                         <h2 className="h2-1">E-Ticaret Sitesi</h2>
                         <p className="p-1">E-Ticaret Web Sitesi</p>
                     </div>
                 </a>
                 <a href="https://kocserhatt.github.io/github-card/">
                     <div className="card">   
-                        <img className="img-2" src="/projects/Ekran görüntüsü 2024-04-08 173152.png" alt=""/>
+                        <img className="img-2" src="Ekran görüntüsü 2024-04-08 173152.png" alt=""/>
                         <h2 className="h2-1">Github Card</h2>
                         <p className="p-1">Serhat Koc GitHub Kart</p>
                     </div>
                 </a>
                 <a href="https://kocserhatt.github.io/memory-game/">
                     <div className="card">   
-                        <img className="img-2" src="/projects/Ekran görüntüsü 2024-04-08 173206.png" alt=""/>
+                        <img className="img-2" src="Ekran görüntüsü 2024-04-08 173206.png" alt=""/>
                         <h2 className="h2-1">memory-game</h2>
                         <p className="p-1">Hafıza Oyunu</p>
                     </div>
                 </a>
                 <a href="https://kocserhatt.github.io/akademi-sinav/">
                     <div className="card">   
-                        <img className="img-2" src="/projects/Ekran görüntüsü 2024-04-08 191048.png" alt=""/>
+                        <img className="img-2" src="Ekran görüntüsü 2024-04-08 191048.png" alt=""/>
                         <h2 className="h2-1">responsive-tasarim</h2>
                         <p className="p-1">Figma responsive tasarım çalışması.</p>
                     </div>
                 </a>
                 <a href="https://kocserhatt.github.io/comment-figma/">
                     <div className="card">   
-                        <img className="img-2" src="/projects/Ekran görüntüsü 2024-04-08 173239.png" alt=""/>
+                        <img className="img-2" src="Ekran görüntüsü 2024-04-08 173239.png" alt=""/>
                         <h2 className="h2-1">comment-figma</h2>
                         <p className="p-1">LFigma Yorum Site Çalışması.</p>
                     </div>
